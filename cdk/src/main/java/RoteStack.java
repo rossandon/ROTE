e3ca1632a359c4f8f8bad4e20d36e2f1bf2431fc
Ryan Ossandon <ossandon.ryan@gmail.com>
2023-02-20 13:37:29 -0500

    Add HTTPS to CDK deploy; now working

--- cdk/src/main/java/RoteStack.java
+++ cdk/src/main/java/RoteStack.java
@@ -1,16 +1,23 @@
+import org.jetbrains.annotations.NotNull;
 import software.amazon.awscdk.App;
-import software.amazon.awscdk.SecretsManagerSecretOptions;
+import software.amazon.awscdk.RemovalPolicy;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
-import software.amazon.awscdk.services.ec2.*;
+import software.amazon.awscdk.services.certificatemanager.Certificate;
+import software.amazon.awscdk.services.certificatemanager.CertificateValidation;
+import software.amazon.awscdk.services.ec2.Peer;
+import software.amazon.awscdk.services.ec2.Port;
+import software.amazon.awscdk.services.ec2.SecurityGroup;
+import software.amazon.awscdk.services.ec2.Vpc;
 import software.amazon.awscdk.services.ecr.Repository;
 import software.amazon.awscdk.services.ecs.*;
 import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedFargateService;
 import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedTaskImageOptions;
-import software.amazon.awscdk.services.ecs.patterns.QueueProcessingFargateService;
-import software.amazon.awscdk.services.elasticache.CfnSecurityGroupIngress;
+import software.amazon.awscdk.services.elasticloadbalancingv2.HealthCheck;
+import software.amazon.awscdk.services.msk.alpha.ClusterConfigurationInfo;
 import software.amazon.awscdk.services.msk.alpha.KafkaVersion;
-import software.amazon.awscdk.services.s3.Bucket;
+import software.amazon.awscdk.services.route53.HostedZone;
+import software.amazon.awscdk.services.route53.HostedZoneAttributes;
 import software.amazon.awscdk.services.secretsmanager.Secret;
 
 import java.util.List;
@@ -24,6 +31,11 @@ public class RoteStack extends Stack {
     public RoteStack(final App scope, final String id, final StackProps props) {
         super(scope, id, props);
 
+        var domainName = "rote.apps.ryanossandon.com";
+        var version = "b08cfff";
+        var profile = "prod";
+        var googleOAuthClientId = "162744778869-jkl6qurhtus9mg28gtgpr9mge73qvo8t.apps.googleusercontent.com";
+
         Vpc vpc = Vpc.Builder.create(this, "Vpc")
                 .maxAzs(3)
                 .build();
@@ -31,35 +43,41 @@ public class RoteStack extends Stack {
         Cluster cluster = Cluster.Builder.create(this, "EcsCluster")
                 .vpc(vpc).build();
 
-        Repository repository = Repository.Builder.create(this, "ECR")
-                .repositoryName("rote")
+        var repository = Repository.fromRepositoryArn(this, "ECR", ManualResources.EcrRepoArn);
+
+        var kafkaCluster = kafkaCluster(vpc);
+
+        var hostedZone = HostedZone.fromHostedZoneAttributes(this, "HostedZone", ManualResources.Route53HostedZone);
+
+        var certificate = Certificate.Builder.create(this, "WebServiceCertificate")
+                .certificateName("WebServiceCertificate")
+                .domainName(domainName)
+                .validation(CertificateValidation.fromDns(hostedZone))
                 .build();
 
-        var kafkaCluster = software.amazon.awscdk.services.msk.alpha.Cluster.Builder.create(this, "Kafka")
-                .vpc(vpc)
-                .clusterName("rote")
-                .kafkaVersion(KafkaVersion.V2_6_0)
-                .build();
-
-        var googleOAuthSecret = Secret.fromSecretCompleteArn(this, "GoogleAuthSecret", "arn:aws:secretsmanager:us-east-2:048545230017:secret:GoogleAuth0Secret-Yc5ijL");
-
+        var googleOAuthSecret = Secret.fromSecretCompleteArn(this, "GoogleAuthSecret", ManualResources.GoogleOAuthSecretArn);
         var clientSecretProp = "spring.security.oauth2.client.registration.google.client-secret";
         var clientIdProp = "spring.security.oauth2.client.registration.google.client-id";
-
         var secrets = Map.of(clientSecretProp, software.amazon.awscdk.services.ecs.Secret.fromSecretsManager(googleOAuthSecret));
 
-        ApplicationLoadBalancedFargateService.Builder.create(this, "WebService")
+        var webService = ApplicationLoadBalancedFargateService.Builder.create(this, "WebService")
                 .cluster(cluster)
                 .cpu(512)
                 .desiredCount(2)
+                .domainZone(hostedZone)
+                .domainName(domainName)
+                .certificate(certificate)
                 .taskImageOptions(
                         ApplicationLoadBalancedTaskImageOptions.builder()
-                                .image(ContainerImage.fromEcrRepository(repository, "rote-webService-023f64c"))
+                                .image(ContainerImage.fromEcrRepository(repository, "rote-webService-" + version))
                                 .environment(Map.of(
-                                        "SPRING_PROFILES_ACTIVE", "prod",
+                                        "SPRING_PROFILES_ACTIVE", profile,
                                         "kafka.targetHost", kafkaCluster.getBootstrapBrokersTls(),
-                                        "kafka.environmentName", "prod",
-                                        clientIdProp, "162744778869-jkl6qurhtus9mg28gtgpr9mge73qvo8t.apps.googleusercontent.com"
+                                        "kafka.environmentName", profile,
+                                        "kafka.tls", "true",
+                                        "server.use-forward-headers", "true",
+                                        "server.forward-headers-strategy", "NATIVE",
+                                        clientIdProp, googleOAuthClientId
                                 ))
                                 .secrets(secrets)
                                 .build())
@@ -67,17 +85,49 @@ public class RoteStack extends Stack {
                 .publicLoadBalancer(true)
                 .build();
 
-        QueueProcessingFargateService.Builder.create(this, "TradingEngineService")
-                .cluster(cluster)
-                .cpu(512)
-                .memoryLimitMiB(2048)
-                .image(ContainerImage.fromEcrRepository(repository, "rote-webService-023f64c"))
+        webService.getTargetGroup().configureHealthCheck(HealthCheck.builder().path("/system/ping").build());
+
+        var tradingEngineServiceTaskDef = FargateTaskDefinition.Builder.create(this, "TradingEngineServiceTaskDef")
+                .cpu(1024)
+                .memoryLimitMiB(4096)
+                .build();
+
+        var tradingEngineContainerDef = ContainerDefinitionOptions.builder()
+                .image(ContainerImage.fromEcrRepository(repository, "rote-trading-" + version))
+                .logging(LogDrivers.awsLogs(AwsLogDriverProps.builder().streamPrefix("TradingEngine").build()))
                 .environment(Map.of(
-                        "SPRING_PROFILES_ACTIVE", "prod",
+                        "SPRING_PROFILES_ACTIVE", profile,
                         "kafka.targetHost", kafkaCluster.getBootstrapBrokersTls(),
-                        "kafka.environmentName", "prod",
+                        "kafka.environmentName", profile,
+                        "kafka.tls", "true",
                         "tradingEngineContext.provider", "fresh"
                 ))
                 .build();
+
+        tradingEngineServiceTaskDef.addContainer("main", tradingEngineContainerDef);
+
+        FargateService.Builder.create(this, "TradingEngineService")
+                .desiredCount(1)
+                .cluster(cluster)
+                .taskDefinition(tradingEngineServiceTaskDef)
+                .build();
+    }
+
+    @NotNull
+    private static software.amazon.awscdk.services.msk.alpha.Cluster kafkaCluster(Vpc vpc) {
+        var kafkaSecurityGroup = SecurityGroup.Builder.create(this, "KafkaSecurityGroup")
+                .vpc(vpc)
+                .build();
+
+        kafkaSecurityGroup.addIngressRule(Peer.anyIpv4(), Port.tcp(9094));
+
+        var kafkaCluster = software.amazon.awscdk.services.msk.alpha.Cluster.Builder.create(this, "Kafka")
+                .vpc(vpc)
+                .removalPolicy(RemovalPolicy.DESTROY)
+                .clusterName("rote")
+                .securityGroups(List.of(kafkaSecurityGroup))
+                .kafkaVersion(KafkaVersion.V2_6_0)
+                .build();
+        return kafkaCluster;
     }
 }
